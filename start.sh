a9770d8166fa398c688b79c4ccc9661de2b90a24
teanocrata <teanocrata@gmail.com>
2017-11-11 15:46:15 +0100

    Change wait algorithm to last domain

--- start.sh
+++ start.sh
@@ -68,19 +68,20 @@ else
 
   # This bit waits until the letsencrypt container has done its thing.
   # We see the changes here bceause there's a docker volume mapped.
-  echo Waiting for folder /etc/letsencrypt/live/$OPENSMARTCOUNTRY_DOMAIN to exist
-  while [ ! -d /etc/letsencrypt/live/$OPENSMARTCOUNTRY_DOMAIN ] ;
+  echo Waiting for folder /etc/letsencrypt/live/$HIPICASOLERA_DOMAIN to exist
+  while [ ! -d /etc/letsencrypt/live/$HIPICASOLERA_DOMAIN ] ;
   do
+      echo Waiting for dir /etc/letsencrypt/live/$HIPICASOLERA_DOMAIN to exist
       sleep 2
   done
 
-  while [ ! -f /etc/letsencrypt/live/$OPENSMARTCOUNTRY_DOMAIN/fullchain.pem ] ;
+  while [ ! -f /etc/letsencrypt/live/$HIPICASOLERA_DOMAIN/fullchain.pem ] ;
   do
       echo Waiting for file fullchain.pem to exist
       sleep 2
   done
 
-  while [ ! -f /etc/letsencrypt/live/$OPENSMARTCOUNTRY_DOMAIN/privkey.pem ] ;
+  while [ ! -f /etc/letsencrypt/live/$HIPICASOLERA_DOMAIN/privkey.pem ] ;
   do
     echo Waiting for file privkey.pem to exist
     sleep 2
